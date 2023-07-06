31980bd33198c159072699a4296fa37e229ff543
Luam Ahrion <luam_azf@hotmail.com>
2023-07-05 23:22:56 -0300

    front fullstack incializado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fullstack-front';
+export class AppComponent implements OnInit {
+  title?: string;
+  livros?: any[];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {}
+
+  getLivros() {
+    this.apiService.getLivros().subscribe(
+      (response) => {
+        this.livros = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }

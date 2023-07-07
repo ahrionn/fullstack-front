df9cad764358d18fd6b219698b15c8608b057e3e
Luam Ahrion <luam_azf@hotmail.com>
2023-07-07 11:46:40 -0300

    incluso formulário para envio de novo livro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
+import { Livro } from './livro.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,13 @@ import { ApiService } from './api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  showTable = false;
+  showFormulario = false;
+  novoLivro: Livro = {
+    nome: '',
+    autor: '',
+    ano: 0
+  };
   title: string = '';
   livros: any[] = [];
 
@@ -14,14 +22,32 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {}
 
+  submitForm() {
+    // Lógica para enviar os dados do novo livro para a API
+    // Aqui você pode fazer a chamada HTTP para a sua API e enviar os dados do novo livro
+    // por exemplo, utilizando o serviço HttpClient do Angular.
+    // Você também pode reinicializar a variável novoLivro e definir showForm como false para ocultar o formulário após o envio dos dados.
+  }
+
   getLivros() {
     this.apiService.getLivros().subscribe(
       (response) => {
         this.livros = response;
+        this.showTable = true; // Atualiza a propriedade showTable para exibir a tabela
       },
       (error) => {
         console.error(error);
       }
     );
+  }
+  
+  exibirTabela() {
+    this.showTable = true;
+    this.showFormulario = false;
+  }
+
+  exibirFormulario() {
+    this.showTable = false;
+    this.showFormulario = true;
   }
 }
